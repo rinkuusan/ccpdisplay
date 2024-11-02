25d08d79354de6a6c6450af2b9eeb5049b8d904e
rinkuusan <rinkuusan@gmail.com>
2024-11-02 15:15:20 +0900

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 
 const web3 = new Web3('https://rpc.defi-verse.org/');
-const tokenPairAddress = '0xYourTokenPairAddress';
+const tokenPairAddress = '0x4724b6bfe09a27ea04ada9c341997aa44cbddd8100020000000000000000000b';
 const tokenPairABI = [/* ここにABIを貼り付け */];
 const tokenPairContract = new web3.eth.Contract(tokenPairABI, tokenPairAddress);
 
