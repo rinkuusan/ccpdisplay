ce3a42b7a8cf1ca0477ec899a7ab91615e93385e
rinkuusan <rinkuusan@gmail.com>
2024-11-02 15:15:33 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 // Vercelではポート指定が不要
 const web3 = new Web3('https://rpc.defi-verse.org/');
 
-const tokenPairAddress = '0xYourTokenPairAddress';
+const tokenPairAddress = '0x4724b6bfe09a27ea04ada9c341997aa44cbddd8100020000000000000000000b';
 const tokenPairABI = [/* ここにABIを貼り付け */];
 const tokenPairContract = new web3.eth.Contract(tokenPairABI, tokenPairAddress);
 
